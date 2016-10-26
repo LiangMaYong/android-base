eb39b21080c176cbbe282bde852d96b8d9994aa9
liangmayong <ibeam@qq.com>
2016-10-26 14:53:16 +0800

    v1.1.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 this is android base
 
 [![LatestVersion](https://img.shields.io/badge/LatestVersion-1.1.0-brightgreen.svg?style=plastic) ](https://github.com/LiangMaYong/android-base/releases/)
-## gradle
+## Gradle
 ```
 compile 'com.liangmayong.android:base:$LatestVersion'
 ```
--- base/src/main/java/com/liangmayong/base/utils/FrescoUtils.java
+++ base/src/main/java/com/liangmayong/base/utils/FrescoUtils.java
@@ -1,49 +1,35 @@
 package com.liangmayong.base.utils;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.widget.ImageView;
 
-import com.facebook.binaryresource.FileBinaryResource;
-import com.facebook.cache.common.SimpleCacheKey;
-import com.facebook.common.executors.CallerThreadExecutor;
-import com.facebook.common.references.CloseableReference;
-import com.facebook.datasource.DataSource;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
-import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
-import com.facebook.imagepipeline.image.CloseableImage;
 import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
-import java.io.File;
-import java.io.IOException;
-
 /**
- * Created by LiangMaYong on 2016/8/22.
+ * Created by LiangMaYong on 2016/10/26.
  */
+
 public class FrescoUtils {
 
     private static boolean isInit = false;
 
     /**
-     * loadThumb
+     * showThumb
      *
      * @param draweeView     draweeView
      * @param url            url
      * @param resizeWidthDp  resizeWidth
      * @param resizeHeightDp resizeHeight
      */
-    public static void loadThumb(SimpleDraweeView draweeView, String url, int resizeWidthDp, int resizeHeightDp) {
+    public static void showThumb(SimpleDraweeView draweeView, String url, int resizeWidthDp, int resizeHeightDp) {
         if (url == null || "".equals(url))
             return;
         if (draweeView == null)
@@ -61,63 +47,6 @@ public class FrescoUtils {
     }
 
     /**
-     * getCacheFile
-     *
-     * @param url
-     */
-    public static File getCacheFile(String url) {
-        FileBinaryResource resource = (FileBinaryResource) Fresco.getImagePipelineFactory().getMainDiskStorageCache().getResource(new SimpleCacheKey(Uri.parse(url).toString()));
-        return resource.getFile();
-    }
-
-    /**
-     * getCacheBitmap
-     *
-     * @param url
-     */
-    public static Bitmap getCacheBitmap(String url) {
-        FileBinaryResource resource = (FileBinaryResource) Fresco.getImagePipelineFactory().getMainDiskStorageCache().getResource(new SimpleCacheKey(Uri.parse(url).toString()));
-        try {
-            return BitmapFactory.decodeStream(resource.openStream());
-        } catch (IOException e) {
-        }
-        return null;
-    }
-
-    /**
-     * load
-     *
-     * @param imageView imageView
-     * @param url       url
-     */
-    public static void load(final ImageView imageView, String url) {
-        if (imageView == null) {
-            return;
-        }
-        ImageRequest imageRequest = ImageRequestBuilder
-                .newBuilderWithSource(Uri.parse(url))
-                .setProgressiveRenderingEnabled(true)
-                .build();
-        ImagePipeline imagePipeline = Fresco.getImagePipeline();
-        DataSource<CloseableReference<CloseableImage>>
-                dataSource = imagePipeline.fetchDecodedImage(imageRequest, imageView.getContext());
-        dataSource.subscribe(new BaseBitmapDataSubscriber() {
-                                 @Override
-                                 public void onNewResultImpl(@Nullable Bitmap bitmap) {
-                                     // You can use the bitmap in only limited ways
-                                     // No need to do any cleanup.
-                                     imageView.setImageBitmap(bitmap);
-                                 }
-
-                                 @Override
-                                 public void onFailureImpl(DataSource dataSource) {
-                                     // No cleanup required here.
-                                 }
-                             },
-                CallerThreadExecutor.getInstance());
-    }
-
-    /**
      * initialize
      *
      * @param context context
@@ -131,5 +60,4 @@ public class FrescoUtils {
         Fresco.initialize(context, config);
         isInit = true;
     }
-
 }
