459be1b7c0a4eb16b04f9a2f6bab0bd83fca7711
liangmayong <ibeam@qq.com>
2016-11-28 17:11:31 +0800

    fix status bar bug

--- app/src/main/java/com/liangmayong/android_base/App.java
+++ app/src/main/java/com/liangmayong/android_base/App.java
@@ -1,12 +1,6 @@
 package com.liangmayong.android_base;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
 import com.liangmayong.base.BaseApplication;
-import com.liangmayong.base.utils.ToastUtils;
-import com.liangmayong.base.widget.eternal.Eternal;
 
 /**
  * Created by LiangMaYong on 2016/11/28.
@@ -17,11 +11,5 @@ public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Eternal.init(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                ToastUtils.showToast("Eternal onReceive");
-            }
-        });
     }
 }
